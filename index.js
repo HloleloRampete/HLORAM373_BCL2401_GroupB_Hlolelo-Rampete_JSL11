7cfa443cb891917603d0020216274deb896af1da
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-04-23 09:56:42 +0200

    Fixed 'styleActiveBoard()' function bug

--- index.js
+++ index.js
@@ -131,13 +131,13 @@ function refreshTasksUI() {
 // Styles the active board by adding an active class
 // TASK: Fix Bugs
 function styleActiveBoard(boardName) {
-  document.querySelectorAll('.board-btn').foreach(btn => { 
-    
+  [...document.querySelectorAll('.board-btn')].forEach(btn => { // the .forEach' method does not return a 'Nodelist'. Therefore, converted the NodeList to an array using the spread operator (...).
+    // The Element.add and Element.remove methods require a string argument representing the class name
     if(btn.textContent === boardName) {
-      btn.add('active') 
+      btn.classList.add('active') 
     }
     else {
-      btn.remove('active'); 
+      btn.classList.remove('active'); 
     }
   });
 }
