ceeb546a099accfad55811a16dc192d0f7e4c6a7
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-04-24 11:04:49 +0200

    Adding the tasks container

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function displayBoards(boards) {
     const boardElement = document.createElement("button");
     boardElement.textContent = board;
     boardElement.classList.add("board-btn");
-    boardElement.addEventListener("click", function() { // Added an anonymous function here and wrapped the event handler code with the function 
+    boardElement.addEventListener("click", () => { // Added an anonymous function here and wrapped the event handler code with the function 
       /* Instead of assigning the click event handler 
     inside the forEach loop, you are immediately invoking the function. */
       elements.headerBoardName.textContent = board;
@@ -134,6 +134,7 @@ function filterAndDisplayTasksByBoard(boardName) {
                         </div>`;
 
     const tasksContainer = document.createElement("div");
+    tasksContainer.classList.add('tasks-container');
     column.appendChild(tasksContainer);
 
     filteredTasks.filter(task => task.status === status).forEach(task => { /* replace the assignment operator with the equality operator to avoid
